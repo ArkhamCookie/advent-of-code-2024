4ec0e027ef8e5a576ff3aca78145ff3a6aae7297
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-12-01 14:35:33 -0600

    feat: add reading of file

--- rust/day-1/src/main.rs
+++ rust/day-1/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+	let path = match env::args().nth(1) {
+		Some(arg) => arg,
+		None => {
+			println!("Missing required arguement: <input path>");
+			exit(1);
+		}
+	};
+	let file = match fs::read_to_string(path) {
+		Ok(file) => file,
+		Err(error) => {
+			println!("[ERROR] Issue reading file: {}", error);
+			exit(1);
+		}
+	};
+
+	println!("{}", file);
 }
