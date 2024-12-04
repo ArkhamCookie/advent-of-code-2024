6d64587b640dfda688a78d3e3d49ee04f8a633e4
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-12-04 15:42:57 -0600

    feat: get difference between numbers

--- rust/day-2/src/main.rs
+++ rust/day-2/src/main.rs
@@ -1,9 +1,56 @@
-use utils::input_file;
+use utils::{difference, input_file, parse};
 
 mod utils;
+
+fn part_one(input: &str) -> (i64, Vec<i64>) {
+	let (column_one, column_two, column_three, column_four, column_five) = parse(input);
+	let mut safe_amount = 0;
+	let mut safe_reports: Vec<i64> = Vec::new();
+
+	for i in 0..column_one.len() {
+		let mut difference_amount: i64;
+
+		// Comparing 1st to 2nd
+		difference_amount = difference(column_one[i], column_two[i]);
+
+		if difference_amount > 3 && difference_amount != 0 {
+			continue;
+		}
+
+		// Comparing 2nd to 3rd
+		difference_amount = difference(column_two[i], column_three[i]);
+
+		if difference_amount > 3 && difference_amount != 0 {
+			continue;
+		}
+
+		// Comparing 3rd to 4th
+		difference_amount = difference(column_three[i], column_four[i]);
+
+		if difference_amount > 3 && difference_amount != 0 {
+			continue;
+		}
+
+		// Comparing 4th to 5th
+		difference_amount = difference(column_four[i], column_five[i]);
+
+		if difference_amount > 3 && difference_amount != 0 {
+			continue;
+		}
+
+		safe_reports.push((i + 1).try_into().unwrap());
+		safe_amount += 1;
+	}
+
+	(safe_amount, safe_reports)
+}
 
 fn main() {
 	let file = input_file();
 
-	println!("File:\n{}", file);
+	let (part_one_solution, _safe_reports) = part_one(&file);
+
+	println!("Part 1: {}", part_one_solution);
+	#[cfg(debug_assertions)]
+	println!("Safe Reports: {:?}", _safe_reports);
 }
--- rust/day-2/src/utils.rs
+++ rust/day-2/src/utils.rs
@@ -19,3 +19,41 @@ pub(crate) fn input_file() -> String {
 		}
 	}
 }
+
+#[allow(clippy::type_complexity)]
+pub(crate) fn parse(input: &str) -> (Vec<i64>, Vec<i64>, Vec<i64>, Vec<i64>, Vec<i64>) {
+	let mut column_one: Vec<i64> = Vec::new();
+	let mut column_two: Vec<i64> = Vec::new();
+	let mut column_three: Vec<i64> = Vec::new();
+	let mut column_four: Vec<i64> = Vec::new();
+	let mut column_five: Vec<i64> = Vec::new();
+
+	for line in input.lines() {
+		if line.is_empty() {
+			continue;
+		}
+
+		let numbers: Vec<&str> = line.split_whitespace().collect();
+		let first_number: i64 = numbers[0].parse().unwrap();
+		let second_number: i64 = numbers[1].parse().unwrap();
+		let third_number: i64 = numbers[2].parse().unwrap();
+		let fourth_number: i64 = numbers[3].parse().unwrap();
+		let fifth_number: i64 = numbers[4].parse().unwrap();
+
+		column_one.push(first_number);
+		column_two.push(second_number);
+		column_three.push(third_number);
+		column_four.push(fourth_number);
+		column_five.push(fifth_number);
+	}
+
+	(column_one, column_two, column_three, column_four, column_five)
+}
+
+pub(crate) fn difference(number_one: i64, number_two: i64) -> i64 {
+	if number_one > number_two {
+		number_one - number_two
+	} else {
+		number_two - number_one
+	}
+}
