0363321f2cd08b77d876ef034b3da70e9a8a0e39
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-12-10 13:38:59 -0600

    feat: create shared library for reused functions

---
+++ rust/shared/src/lib.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs;
+use std::process::exit;
+
+pub fn input_file() -> String {
+	let path = match env::args().nth(1) {
+		Some(arg) => arg,
+		None => {
+			println!("Missing required arguement: <input path>");
+			exit(1);
+		}
+	};
+
+	match fs::read_to_string(path) {
+		Ok(file) => file,
+		Err(error) => {
+			println!("[ERROR] Issue reading file: {}", error);
+			exit(1);
+		}
+	}
+}
